6d726ef1c89f5b620ac02dee8f4cad2f9f8ff25e
Joseph Anthony Meneses Salguero <68487005+ElJoamy@users.noreply.github.com>
2021-08-17 14:33:42 -0400

    Create queen 8.cpp

---
+++ algoritmos/Backtracking/queen 8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+using namespace std;
+int search(int i)
+{
+	for(int j=1;j<=8;j++)
+	{
+		if(Se puede colocar)
+		{
+			 Lugar reina
+			 Posición de la marca
+			if(i==8)  Salida
+			else search(i+1);
+			 Marca dónde colocar la reina, ¿puedes probar la siguiente posición?
+		}
+	}
+}
